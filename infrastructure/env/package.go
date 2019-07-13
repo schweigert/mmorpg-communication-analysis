3a2b32c67b761da415a0b9c61ec8c010791e0d1a
schweigert <marlon.henry@magrathealabs.com>
2019-07-13 14:32:59 -0300

    Interface and port envs

--- infrastructure/env/package.go
+++ infrastructure/env/package.go
@@ -9,10 +9,24 @@ import (
 // Defaults
 const (
 	DefaultServiceName = "noname"
+	DefaultInterface   = "0.0.0.0"
+	DefaultPort        = "80"
 )
 
 // ServiceName $SERVICE_NAME
 func ServiceName() string {
 	def := os.Getenv("SERVICE_NAME")
 	return ternary.String(def == "", DefaultServiceName, def)
+}
+
+// Interface $INTERFACE
+func Interface() string {
+	def := os.Getenv("INTERFACE")
+	return ternary.String(def == "", DefaultInterface, def)
+}
+
+// Port $PORT
+func Port() string {
+	def := os.Getenv("PORT")
+	return ternary.String(def == "", DefaultPort, def)
 }
--- infrastructure/env/package_test.go
+++ infrastructure/env/package_test.go
@@ -14,8 +14,24 @@ func (suite *PackageSuite) TestDefaultServiceName() {
 	suite.Equal("noname", DefaultServiceName)
 }
 
+func (suite *PackageSuite) TestDefaultInterface() {
+	suite.Equal("0.0.0.0", DefaultInterface)
+}
+
+func (suite *PackageSuite) TestDefaultPort() {
+	suite.Equal("80", DefaultPort)
+}
+
 func (suite *PackageSuite) TestServiceName() {
 	suite.Equal(DefaultServiceName, ServiceName())
+}
+
+func (suite *PackageSuite) TestInterface() {
+	suite.Equal(DefaultInterface, Interface())
+}
+
+func (suite *PackageSuite) TestPort() {
+	suite.Equal(DefaultPort, Port())
 }
 
 func TestPackageSuite(t *testing.T) {
